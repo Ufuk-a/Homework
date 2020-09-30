fdbd7583ec1ed14730da1b4be93712d40e42ff2a
Ufuka <61376126+Ufuk-a@users.noreply.github.com>
2020-09-30 08:48:59 +0300

    Made it a function

--- soru2.py
+++ soru2.py
@@ -1,15 +1,22 @@
-story = """Güneşli bir yaz günüymüş, 
+
+def first_letterinator_9000(writing, first):    
+    word_list = writing.split(" ")
+    counter = 0
+    for word in word_list:
+        if word[0] == first or word[0] == upper(first):
+            print(word)
+            counter += 1
+        
+    print(f"\nToplam {counter} kelime {letter} ile baslıyordu.")        
+    
+    story = """Güneşli bir yaz günüymüş, 
 anne ördek gölün kenarındaki ağacın altında yumurtlamak için güzel bir yer bulmuş.
 Beş yumurta yumurtlamış, birden yumurtalarından birinin diğerinden farklı olduğunu fark etmiş. 
 Biraz endişelenmiş, sonra yumurtaların çatlamasını beklemiş. 
 Güzel bir sabah, nihayet birbiri peşi sıra yumurtalar kırılmaya başlamış. 
 Cik, cik demişler bütün yumurtalar canlanmış, ördek yavruları başlarını büyük dünyaya doğru uzatıyorlarmış."""
 
-word_list = story.split(" ")
-counter = 0
-for word in word_list:
-    if word[0] == "b" or word[0] == "B":
-        print(word)
-        counter += 1
-        
-print(f"\nToplam {counter} kelime b ile baslıyordu.")        
+
+letter = input("What letter do you want to search for?")
+
+first_letterinator_9000(story, letter)
